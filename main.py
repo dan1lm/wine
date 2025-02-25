7b6f3522d0234de4a60c537be1b95499a400fb53
Danil Merinov <danilmerinov9@gmail.com>
2025-02-25 00:11:13 -0500

    main.py edit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from pipeline_test import logger
+from pipeline_test.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
 
 logger.info("custom logging")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
