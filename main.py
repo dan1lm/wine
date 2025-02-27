c567d3948947c3ab56326ab259b4276bf89d0f5b
Danil Merinov <danilmerinov9@gmail.com>
2025-02-26 23:02:13 -0500

    validation

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from pipeline_test import logger
 from pipeline_test.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from pipeline_test.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 logger.info("custom logging")
-STAGE_NAME = "Data Ingestion stage"
+
+STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
