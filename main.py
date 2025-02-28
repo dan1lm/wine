2b91752d3fe8dca418f3ac939194efb537364962
Danil Merinov <danilmerinov9@gmail.com>
2025-02-28 15:30:44 -0500

    added data transformation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from pipeline_test import logger
 from pipeline_test.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from pipeline_test.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
-
+from pipeline_test.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 logger.info("custom logging")
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -20,6 +19,18 @@ STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
